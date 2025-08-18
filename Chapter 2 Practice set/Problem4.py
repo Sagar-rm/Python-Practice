4ebe0261918930d73372cdd43906fa61e8d932d4
Sagar-rm <sagarrathod292007@gmail.com>
2025-08-18 15:28:25 +0530

    Practicing Python and learning Chapter 2

---
+++ Chapter 2 Practice set/Problem4.py
@@ -0,0 +1,4 @@
+a = int(input("Enter the first number u want compare :"))
+b = int(input("Enter the second number u want compare :"))
+
+print("a is grater than b is ", a  > b)
---
+++ Chapter 2 Practice set/Problem6.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number to find Square :"))
+
+print("The Square of this number is ", a *a)
+print("The Square of this number is ", a **2)
