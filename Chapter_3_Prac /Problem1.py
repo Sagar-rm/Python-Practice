9326c6ddb696de3383c355ea0eae57e93fd1fc1c
Sagar-rm <sagarrathod292007@gmail.com>
2025-08-19 21:47:04 +0530

    Chapter 3 Problem Solved

---
+++ Chapter_3_Prac /Problem1.py
@@ -0,0 +1,2 @@
+a = str(input("Enter Your name :"))
+print(f"Hello Good After Noon {a} How are u !")
---
+++ Chapter_3_Prac /Problem2.py
@@ -0,0 +1,7 @@
+letter = '''
+Dear <|Name|>,
+You are selected!
+<|Date|>
+'''
+
+print(letter.replace("<|Name|>" , "Sagar").replace("<|Date|>" , "12 Sept 2024"))
